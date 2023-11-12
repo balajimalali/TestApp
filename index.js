b75afeba4a900c1929e9506a39c3cac4eb68e42d
balajimalali <balajigovindmalali@gmail.com>
2023-11-12 10:44:14 +0530

    frontend served with express

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var cors = require('cors')
 const express = require('express')
 const dotenv = require('dotenv')
-
+const path = require("path");
 
 dotenv.config();
 const PORT = process.env.PORT || 8080
@@ -12,6 +12,9 @@ app.use(cors({
     credentials: true,
 }))
 app.use(express.json())
+
+app.use(express.static(path.join(__dirname, "frontend", "build")));
+
 app.use('/api', require('./routes/auth'))
 app.use('/api/test', require('./routes/qpaper'))
 app.use('/api/result', require('./routes/eval'))
